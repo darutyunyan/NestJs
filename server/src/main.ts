151bc98e093427829c65dade5bbfe64054043436
daniel <arutyunyan.daniel23@mail.ru>
2021-04-14 15:30:03 +0300

    Adds Cores and Helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
+
+        // Cors.
+        app.enableCors();
+        
+        // Helmet;
+        app.use(helmet());
 
         await app.listen(PORT);
     } catch (e) {
